bd1db17d9db83a86dadde3da9f1cb2563ce59eb8
Toby Qin <toby.qin@live.com>
2015-11-12 15:43:37 +0800

    Script to send email

---
+++ PowerShell/Send-Email.ps1
@@ -0,0 +1,12 @@
+﻿function Send-Email($to, $cc, $subject, $body)
+{
+    $From = "you@domain.com"
+    $SmtpHost = "smtp.domain.com"
+    Send-MailMessage -From $From -Subject $subject -To $to -Cc $cc -Body $body -BodyAsHtml -SmtpServer $SmtpHost
+}
+
+$To = "user1@domain.com", "user2@domain.com"
+$Cc="boss@domain.com"
+$Subject = "Don't worry, it is a test."
+$Body = ":-)"
+Send-Email -to $To -cc $Cc -subject $Subject -body $Body
