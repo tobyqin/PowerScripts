dc635a4aae195303ff66ceb35f7e90fa414550cc
Toby Qin <toby.qin@live.com>
2015-11-12 15:40:31 +0800

    Sample script to get parent calling function info

---
+++ PowerShell/Get-CallingFunctionName.ps1
@@ -0,0 +1,44 @@
+﻿$ErrorActionPreference="Stop"
+$DebugPreference="Continue"
+
+trap
+{
+    Write-Debug "=== Enter trap ==="
+    
+    # C:\GitHub\PowerScripts\PowerShell\Get-CallingFunctionName.ps1
+    #$MyInvocation.InvocationName
+    
+    # Get-CallingFunctionName.ps1
+    #$MyInvocation.MyCommand.Name
+    
+    # Say-Hello
+    #(Get-PSCallStack)[1].Position.Text
+    
+    $funcName = (Get-PSCallStack)[1].Position.Text
+    $detail =  $_ | Out-String      
+    
+    Send-Error $funcName $detail
+    throw "<<<Error occurred, please take a look!>>> `n$detail"
+    
+    Write-Debug "=== Exit trap ===="
+}
+
+function Send-Error ($functionName, $errorDetail)
+{
+    $message = "Function <{0}> was failed, please check. `n=============`n{1}`n=============" -f $functionName, $errorDetail
+    Write-Debug $message
+}
+
+function Say-Hello
+{
+    echo "Hello!"
+    bad-command
+}
+
+function Other-Action
+{
+    Write-Host "Something should not be run if error occurred!" -ForegroundColor Yellow
+}
+
+Say-Hello
+Other-Action
