243898baa5f7df2238e2814434a0ef9b138fbbee
Toby Qin <toby.qin@live.com>
2015-12-22 16:31:50 +0800

    PowerShell logger functions, able to log info, warnning and error.

---
+++ PowerShell/Write-Log.ps1
@@ -0,0 +1,38 @@
+﻿function Log-Info($message, $toFile)
+{
+	$now = {Get-Date -f "HH:mm:ss"}
+	$msg = "[INFO]{0} {1}" -f (&$now), $message
+	
+	if($toFile -ne $null -and (Test-Path $toFile))
+	{
+		$msg | Out-File $toFile -Append
+	}
+
+	Write-Debug $msg
+}
+
+function Log-Warn($message, $toFile)
+{
+	$now = {Get-Date -f "HH:mm:ss"}
+	$msg = "[WARN]{0} {1}" -f (&$now), $message
+	
+	if($toFile -ne $null -and (Test-Path $toFile))
+	{
+		$msg | Out-File $toFile -Append
+	}
+
+	Write-Warning $msg
+}
+
+function Log-Error($message, $toFile)
+{
+	$now = {Get-Date -f "HH:mm:ss"}
+	$msg = "[ERROR]{0} {1}" -f (&$now), $message
+	
+	if($toFile -ne $null -and (Test-Path $toFile))
+	{
+		$msg | Out-File $toFile -Append
+	}
+
+	Write-Error $msg
+}
