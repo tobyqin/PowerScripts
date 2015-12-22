b79c9c3df60890f441e6c0ca1253a3febfe7cee6
Toby Qin <toby.qin@live.com>
2015-12-22 16:33:02 +0800

    A robost copy function by PowerShell.

---
+++ PowerShell/Robo-Copy.ps1
@@ -0,0 +1,25 @@
+﻿function Robo-Copy($from, $to, $log)
+{
+    $tool = "robocopy"
+    $args = "$from $to /MIR /MT:50 /R:0"
+    Log-Info "Tool: '$tool'"
+    Log-Info "Argument: '$args'"   
+
+    Set-Location $env:TEMP
+    $tempLog = [System.IO.Path]::GetTempFileName()
+    Start-Process -FilePath $tool -ArgumentList $args -WindowStyle Hidden -Wait -RedirectStandardOutput $tempLog
+    if($log)
+    {
+        Get-Content $tempLog -ReadCount 0 | Out-File $log -Append
+    }
+    else
+    {
+        Get-Content $tempLog -ReadCount 0
+    }
+
+    if($LastExitCode -ne $null)
+    {
+        Log-Info "Robocopy Exit Code: $LastExitCode" $log
+    }
+    Compare-Folder $from $to $log
+}
