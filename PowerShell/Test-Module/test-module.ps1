201014e051019e8ea6435f835fe3be9d1b7c644b
Toby Qin <toby.qin@live.com>
2015-12-22 16:30:07 +0800

    A sample to use PowerShell module.

---
+++ PowerShell/Test-Module/test-module.ps1
@@ -0,0 +1,16 @@
+﻿trap {
+    "Script trap entered: $_"
+    # exit -1
+    continue
+}
+
+$dir = Split-Path -Parent $PSCommandPath
+
+"Current directory: $dir"
+
+Remove-Module -Force "Test-Module"
+Import-Module $dir
+
+test-function
+
+
---
+++ PowerShell/Test-Module/test-module.psm1
@@ -0,0 +1,16 @@
+﻿   
+function test-function
+{
+    Write-Warning $ErrorActionPreference
+    "Hello, test-function!"
+    bad-command
+}
+
+
+trap {
+    "Module trap entered: $_"
+    # exit -1
+    continue
+}
+
+
