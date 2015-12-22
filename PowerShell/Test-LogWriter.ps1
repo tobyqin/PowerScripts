54f1c2a14d21dd774df57b503256fc0c664bfcd5
Toby Qin <toby.qin@live.com>
2015-12-22 16:40:39 +0800

    PowerShell to write log file.

---
+++ PowerShell/Test-LogWriter.ps1
@@ -0,0 +1,19 @@
+﻿$DebugPreference = "Continue"
+$ErrorActionPreference="Stop"
+
+function Write-Log($logFile, $logInfo)
+{
+	$now = {Get-Date -f "yyyy-MM-dd HH:mm:ss"}
+	$msg = "{0} {1}" -f (&$now), $logInfo
+	
+	if(Test-Path $logFile)
+	{
+		$msg | Out-File $logFile -Append
+	}
+
+	Write-Debug $msg
+}
+
+$log="C:\Test\test.txt"
+
+Write-Log $log "Hello world!"
